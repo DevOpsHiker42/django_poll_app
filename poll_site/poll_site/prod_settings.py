6c8f195f7b34f1e7a590fde9f4bb51e7a12a692f
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-09-24 08:41:44 +0100

    fix CSRF settings

--- poll_site/poll_site/prod_settings.py
+++ poll_site/poll_site/prod_settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['sky']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+'.cloudfront.net',
+'.amazonaws.com'
+]
 
 
 # Application definition
